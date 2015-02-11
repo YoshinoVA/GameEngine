b731bd284ebc46361938ad8a96e028b62c5e434a
YoshinoVA <pokemongx2011@live.com>
2015-02-11 12:23:57 -0800

    picture thing

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -8,33 +8,29 @@
 #include "glm/gtc/type_ptr.hpp"
 #include <fstream>
 #include <iostream>
+#include <ctime>
 
 //vertex shader
 const GLchar* vertexSource =
-"#version 330\n"
-"in vec2 vPosition;"
-"in vec2 texcoord;"
+"#version 150 core\n"
+"in vec2 position;"
 "in vec3 color;"
+"in vec2 texcoord;"
 "out vec3 Color;"
 "out vec2 Texcoord;"
-"uniform mat4 trans;"
-"void main()"
-"{"
-"Texcoord = texcoord;"
-"	Color = color;"
-"	gl_Position = vec4(vPosition, 0.0, 1.0);"
+"void main() {"
+"   Color = color;"
+"   Texcoord = texcoord;"
+"   gl_Position = vec4(position, 0.0, 1.0);"
 "}";
-
-//fragment shader
 const GLchar* fragmentSource =
-"#version 330 \n"
+"#version 150 core\n"
 "in vec3 Color;"
 "in vec2 Texcoord;"
 "out vec4 outColor;"
 "uniform sampler2D tex;"
-"void main()"
-"{"
-"	outColor = texture(tex, Texcoord) * vec4(Color, 1.0);"
+"void main() {"
+"   outColor = texture(tex, Texcoord) * vec4(Color, 1.0);"
 "}";
 
 //test shaders
@@ -56,7 +52,15 @@ static bool printShaderInfoLog(GLuint obj)
 	}
 	return true;
 }
+float radianToDegrees(float radians)
+{
+	float returnNumber;
 
+	// degrees to radian
+	returnNumber = radians * 180 / 3.14;
+
+	return returnNumber;
+}
 int main()
 {
 	glfwInit();
@@ -75,8 +79,6 @@ int main()
 
 	glewExperimental = GL_TRUE;
 	glewInit();
-	
-	glGenerateMipmap(GL_TEXTURE_2D);
 
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
@@ -86,8 +88,8 @@ int main()
 	glGenBuffers(1, &vbo);
 
 	// math and numbers and stuff
-	float vertices[] = {
-		//  Position      Color             Texcoords
+	GLfloat vertices[] = {
+		//  Position   Color             Texcoords
 		-0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
 		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
 		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
@@ -101,13 +103,6 @@ int main()
 		0, 1, 2,
 		2, 3, 0
 	};
-
-	float pixels[] = {
-		0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f
-	};
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -128,7 +123,7 @@ int main()
 	glLinkProgram(shaderProgram);
 	glUseProgram(shaderProgram);
 
-	GLint posAttrib = glGetAttribLocation(shaderProgram, "vPosition");
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
 	glEnableVertexAttribArray(posAttrib);
 	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), 0);
 
@@ -143,20 +138,16 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
-	GLint uniTrans = glGetUniformLocation(shaderProgram, "trans");
-
 	GLuint tex;
 	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
 
 	int width, height;
-	unsigned char* image =
-	SOIL_load_image("Ba Sing Se.png", &width, &height, 0, SOIL_LOAD_RGB);
+	unsigned char* image = SOIL_load_image("Western Air Temple.png", &width, &height, 0, SOIL_LOAD_RGB);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
@@ -181,24 +172,22 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		// Clear the screen to black
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glm::mat4 trans;
-		trans = glm::rotate(trans, 3.14159f, glm::vec3(0.0f, 0.0f, 1.0f));
-		glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(trans));
-		glm::vec4 result = trans * glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-		printf("%f, %f, %f\n", result.x, result.y, result.z);
-
+		// Draw a rectangle from the 2 triangles using 6 indices
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
 	glDeleteProgram(shaderProgram);
 	glDeleteShader(fragmentShader);
 	glDeleteShader(vertexShader);
 
+	glDeleteBuffers(1, &ebo);
 	glDeleteBuffers(1, &vbo);
 
 	glDeleteVertexArrays(1, &vao);
